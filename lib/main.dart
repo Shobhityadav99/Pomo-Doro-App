50f237fa5f174474510fe781261a83b27b18b65f
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-03 17:06:57 +0530

    Percentage indicator in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,13 +15,50 @@ class PomoDoro extends StatefulWidget {
 
 class _PomoDoroState extends State<PomoDoro> {
   double percent = 0;
-  static int timeInmin = 60;
+  static int timeInmin = 25;
   int timeInsec = timeInmin * 60;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xff1542bf), Color(0xff51a8ff)],
+                begin: FractionalOffset(0.5, 1)),
+          ),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 25.0),
+                child: Text(
+                  'Pomodoro Clock',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: CircularPercentIndicator(
+                  percent: percent,
+                  animation: true,
+                  animateFromLastPercent: true,
+                  radius: 250.0,
+                  lineWidth: 20.0,
+                  progressColor: Colors.white,
+                  center: Text(
+                    "$timeInmin",
+                    style: TextStyle(color: Colors.white, fontSize: 80.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }

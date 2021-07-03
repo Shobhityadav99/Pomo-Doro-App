8a527cd63d369877acf5b51ce60ad7966ea5f35f
Shobhit Yadav <shobhit999000@gmail.com>
2021-07-03 17:45:01 +0530

    timer working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -17,6 +19,34 @@ class _PomoDoroState extends State<PomoDoro> {
   double percent = 0;
   static int timeInmin = 25;
   int timeInsec = timeInmin * 60;
+  Timer timer;
+  __startTimer() {
+    timeInmin = 25;
+    int time = timeInmin * 60;
+    double secPercentage = (time / 100);
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (time > 0) {
+          time--;
+          if (time % 60 == 0) {
+            timeInmin--;
+          }
+          if (time % secPercentage == 0) {
+            if (percent < 1) {
+              percent += 0.01;
+            } else {
+              percent = 1;
+            }
+          }
+        } else {
+          percent = 0;
+          timeInmin = 25;
+          timer.cancel();
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -53,6 +83,83 @@ class _PomoDoroState extends State<PomoDoro> {
                   center: Text(
                     "$timeInmin",
                     style: TextStyle(color: Colors.white, fontSize: 80.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0))),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+                    child: Column(
+                      children: [
+                        Expanded(
+                            child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Study Timer",
+                                    style: TextStyle(fontSize: 30.0),
+                                  ),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  Text(
+                                    "25",
+                                    style: TextStyle(fontSize: 80.0),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Pause Timer",
+                                    style: TextStyle(fontSize: 30.0),
+                                  ),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  Text(
+                                    "5",
+                                    style: TextStyle(fontSize: 80.0),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        )),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: RaisedButton(
+                            color: Colors.blue,
+                            onPressed: __startTimer,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100.0)),
+                            child: Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                "Start Studying",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 22.0),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               )
